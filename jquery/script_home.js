08472d81a9e158f1986b070551cce54a01be9cd7
wehmam <readytosurff@gmail.com>
2020-02-06 13:20:25 +0700

    Selesai home

--- jquery/script_home.js
+++ jquery/script_home.js
@@ -1,8 +1,37 @@
 $(document).ready(function(){
 
+    // $('.gambar1').mouseenter(function(){
+    //     $('.text').slideUp()
+    //     }).mouseleave(function(){
+    //         $('.text').slideDown()
+    // });
+    $('.text, .text2, .text3, .text4').hide();
+    
     $('.gambar1').mouseenter(function(){
+        $('.text').slideDown('slow')
+    });
+    $('.gambar1').mouseleave(function(){
         $('.text').slideUp()
-        }).mouseleave(function(){
-            $('.text').slideDown()
+    });
+
+    $('.gambar2').mouseenter(function(){
+        $('.text2').slideDown('slow')
+    });
+    $('.gambar2').mouseleave(function(){
+        $('.text2').slideUp()
+    });
+
+    $('.gambar3').mouseenter(function(){
+        $('.text3').slideDown('slow')
+    });
+    $('.gambar3').mouseleave(function(){
+        $('.text3').slideUp()
+    });
+
+    $('.gambar4').mouseenter(function(){
+        $('.text4').slideDown('slow')
+    });
+    $('.gambar4').mouseleave(function(){
+        $('.text4').slideUp()
     });
 })
